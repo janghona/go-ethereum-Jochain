e296beadf294679ab66c22ba81bd380cbe56ed81
janghona <jang1na2@gmail.com>
2021-08-30 00:33:05 +0900

    Initial commint (초기 커밋)
    * go-ethereum private network 초기 커밋
    * dapp은 웹을 이용한 간단한 커뮤니티 구성

---
+++ dapp/README.md
@@ -0,0 +1,11 @@
+# product dApp
+
+![](/img/preview.gif)
+
+### Requirement
+
+- Geth
+- Visual Code, Atom or etc..
+- knowledge of javaScript
+
+[해당 dApp Tutorial](https://developer-mac.tistory.com/85)
---
+++ dapp/js/app.js
@@ -0,0 +1,76 @@
+typeof web3 !== "undefined"
+  ? (web3 = new Web3(web3.currentProvider))
+  : (web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545")));
+
+if (web3.isConnected()) {
+  console.log("connected");
+} else {
+  console.log("not connected");
+  exit;
+}
+
+const contractAddress = "0x69c9159874b24e321D2E4096526eF90B6DB51461";
+const smartContract = web3.eth.contract(abi).at(contractAddress);
+
+function init() {
+  // 첫번째 계정으로 주소 설정
+  $("#account").val(web3.eth.accounts[0]);
+  moment.locale();
+}
+
+async function showList() {
+  const table = document.getElementById("table1");
+  const total = smartContract.getTotal().toString();
+
+  smartContract.product().watch(async (err, res) => {
+    if (!err) {
+      const row = table.insertRow();
+      const cell1 = row.insertCell(0);
+      const cell2 = row.insertCell(1);
+      const cell3 = row.insertCell(2);
+      const cell4 = row.insertCell(3);
+
+      const time = (await res.args.timestamp.toString()) * 1000;
+      cell1.innerHTML = await res.args.productName;
+      cell2.innerHTML = await res.args.location;
+      cell3.innerHTML = await res.args.count.toString();
+      cell4.innerHTML = moment(time).format("YYYY-MM-DD hh:mm");
+    }
+  });
+
+  console.log(total);
+  for (let i = 0; i < total; i++) {
+    const product = await smartContract.getProduct(i);
+    const toString = await product.toString();
+    const strArray = toString.split(",");
+
+    const time = strArray[3] * 1000;
+    const row = table.insertRow();
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    const cell4 = row.insertCell(3);
+    cell1.innerHTML = strArray[0];
+    cell2.innerHTML = strArray[1];
+    cell3.innerHTML = strArray[2];
+    cell4.style.width = "60%";
+    cell4.innerHTML = moment(time).format("YYYY-MM-DD hh:mm");
+  }
+}
+
+function addProduct() {
+  smartContract.addProduct(
+    $("#proname").val(),
+    $("#proloc").val(),
+    $("#pronumber").val(),
+    { from: $("#account").val(), gas: 3000000 },
+    (err, result) => {
+      if (!err) alert("트랜잭션이 성공적으로 전송되었습니다.\n" + result);
+    }
+  );
+}
+
+$(function() {
+  init();
+  showList();
+});
